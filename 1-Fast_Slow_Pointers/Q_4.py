f6542a33604679470dbc78caaf898cb377f5418a
Henry Tirla <henrytirla@gmail.com>
2024-06-11 14:30:49 +0200

    Q_4 Section 2

--- 1-Fast_Slow_Pointers/Q_4.py
+++ 1-Fast_Slow_Pointers/Q_4.py
@@ -7,4 +7,39 @@ The length of the sequence is at least two.
 The loop must be in a single direction, forward or backward.
 Note: A cycle in the array does not have to originate at the beginning. It may begin from any point in the array.
 
-Your task is to determine if nums has a cycle. Return TRUE if there is a cycle. Otherwise return FALSE."""
+Your task is to determine if nums has a cycle. Return TRUE if there is a cycle. Otherwise return FALSE.
+
+"""
+
+class Solution:
+    def circular_array_loop(self, nums: list[int]) -> bool:
+        n = len(nums)
+        for i in range(n):
+            slow, fast = i, i
+            direction = nums[i] > 0
+            while True:
+                slow = (slow + nums[slow]) % n
+                fast = (fast + nums[fast]) % n
+                if (nums[fast] > 0) != direction:
+                    break
+                fast = (fast + nums[fast]) % n
+                if (nums[fast] > 0) != direction:
+                    break
+                if slow == fast:
+                    if slow == (slow + nums[slow]) % n:
+                        break
+                    return True
+
+        return False
+
+
+def test_circular_array_loop():
+    sol= Solution()
+    assert sol.circular_array_loop([1,3,-2,-4,1]) == True
+    assert sol.circular_array_loop([2,1,-1,-2]) == False
+    assert sol.circular_array_loop([5,4,-2,-1,3]) == False
+    assert sol.circular_array_loop([1,2,-3,3,4,7,1]) == True
+    assert sol.circular_array_loop([3,3,1,-1,2]) == True
+
+if __name__ == '__main__':
+    test_circular_array_loop()
