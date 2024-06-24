310e41b18d7ccdba9953cc4da26f9acd751fa17e
Henry Tirla <henrytirla@gmail.com>
2024-06-24 22:24:23 +0200

    Q_1 Section 3

---
+++ 2-Sliding_Window/Q_1.py
@@ -0,0 +1,12 @@
+"""
+Given a string, s, that represents a DNA subsequence, and a number 𝑘
+, return all the contiguous subsequences (substrings) of length k
+ that occur more than once in the string. The order of the returned subsequences does not matter. If no repeated substring is found, the function should return an empty set.
+
+The DNA sequence is composed of a series of nucleotides abbreviated as
+𝐴A, 𝐶C, 𝐺G, and 𝑇T
+. For example, 𝐴𝐶𝐺𝐴𝐴𝑇𝑇𝐶𝐶𝐺
+ACGAATTCCG
+ is a DNA sequence. When studying DNA, it is useful to identify repeated sequences in it.
+
+ """
