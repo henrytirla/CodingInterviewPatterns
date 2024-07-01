2d881b2de709d7483466c4cdf7b602caa554cac5
Henry Tirla <henrytirla@gmail.com>
2024-07-01 14:54:07 +0200

    Q_6 Section 3

--- 2-Sliding_Window/Q_6.py
+++ 2-Sliding_Window/Q_6.py
@@ -8,17 +8,48 @@ The order of the characters does not matter here.
 
 Note: If there are multiple valid minimum window substrings, return any one of them."""
 
-
+from collections import Counter
 class Solution:
-    def shortest_substring(self,s:str,t:str):
-        pass
+    def min_windows(self,s:str,t:str):
+        if not s or not t or len(s)<len(t):
+            return ""
+
+        frequeny_t = Counter(t)
+        required_chars= len(frequeny_t)
+        formed=0
+        left,right=0,0
+        window_counts={}
+        min_length=float("inf")
+        min_window=""
+        while right<len(s):
+            char = s[right]
+            window_counts[char]=window_counts.get(char,0)+1
+            if char in frequeny_t and window_counts[char]==frequeny_t[char]:
+                formed+=1
+            while left<=right and formed==required_chars:
+                char = s[left]
+                current_length=right-left+1
+                if current_length<min_length:
+                    min_length=current_length
+                    min_window=s[left:right+1]
+                window_counts[char]-=1
+                if char in frequeny_t and window_counts[char]<frequeny_t[char]:
+                    formed-=1
+                left+=1
+            right+=1
+        return min_window
 
 
 
-def test_shortest_substring():
+
+
+
+def test_min_windows():
     sol=Solution()
-    assert sol.shortest_substring("ADOBECODEBANC","ABC")=="BANC"
+    assert sol.min_windows("ADOBECODEBANC","ABC")=="BANC"
+    assert sol.min_windows("ABXYZJKLSNFC","ABC")=="ABXYZJKLSNFC"
+
 
 
 if __name__ == '__main__':
-    test_shortest_substring()
+    test_min_windows()
