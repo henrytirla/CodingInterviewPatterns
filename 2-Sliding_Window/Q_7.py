e96cf02bfdbd6be784abad50dd9708c788240422
Henry Tirla <henrytirla@gmail.com>
2024-07-02 10:49:43 +0200

    Q_7 Section 3

--- 2-Sliding_Window/Q_7.py
+++ 2-Sliding_Window/Q_7.py
@@ -4,3 +4,29 @@
 eg:p,w,w,k,e,w Output_length = 3
 
 """
+
+
+class Solution:
+    def longest_substring(self, input_str: str) -> int:
+        n = len(input_str)
+        start = 0
+        max_length = 0
+        char_index = {}
+        for i in range(n):
+            if input_str[i] in char_index:
+                start = max(start, char_index[input_str[i]] + 1)
+            char_index[input_str[i]] = i
+            max_length = max(max_length, i - start + 1)
+        return max_length
+
+
+def test_longest_substring():
+    sol = Solution()
+    assert sol.longest_substring("abcdbea") == 5
+    assert sol.longest_substring("aba") == 2
+    assert sol.longest_substring("abccabcabcc") == 3
+    assert sol.longest_substring("aaaabaaa") == 2
+    assert sol.longest_substring("bbbbb") == 1
+
+if __name__ == '__main__':
+    test_longest_substring()
