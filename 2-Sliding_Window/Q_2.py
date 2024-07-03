677b0517f3a12701dddf02f4ebddb8cf29314c60
Henry Tirla <henrytirla@gmail.com>
2024-07-03 12:10:37 +0200

    update

--- 2-Sliding_Window/Q_2.py
+++ 2-Sliding_Window/Q_2.py
@@ -11,6 +11,34 @@ ACGAATTCCG
 
  """
 
+#CHAT GPT VERY INTUITIVE EXPLANATION ON HOW AND WHY WINDOWS RANGE IS SET
+"""The loop for i in range(n - k + 1) ensures that you correctly iterate through all possible contiguous substrings of length k within the given string word. Here's an intuitive explanation with an example:
+
+Explanation:
+Given a string word of length n and a desired substring length k:
+
+If you start at index i and want a substring of length k, it will end at index i + k - 1.
+You need to ensure that the substring does not exceed the length of the word.
+Thus, the loop range n - k + 1 ensures that the starting index i only goes up to the point where the substring can still be of length k.
+
+Example:
+Consider word = "abcdefg" and k = 3.
+
+Length of word (n) is 7.
+You want all substrings of length 3.
+The possible substrings of length 3 are:
+
+Start at index 0: "abc"
+Start at index 1: "bcd"
+Start at index 2: "cde"
+Start at index 3: "def"
+Start at index 4: "efg"
+Let's break down the indices:
+
+The first possible start index is 0.
+The last possible start index is n - k which is 7 - 3 = 4.
+So, the loop needs to run from 0 to 4 (inclusive), which is why we use range(n - k + 1), i.e., range(7 - 3 + 1), or range(5)."""
+
 class Solution:
     def find_repeated_sequences(self,s, k):
         sequence_count = {}
