0e974ee5ae7a6e82dd95aed72394b11adb066a43
Henry Tirla <henrytirla@gmail.com>
2024-07-04 11:16:31 +0200

    Merge Intervals -Q_1

---
+++ 3-Merge Intervals/Q_1.py
@@ -0,0 +1,8 @@
+"""We are given an array of closed intervals,
+intervals, where each interval has a start time and an end time.
+The input array is sorted with respect to the start times of each interval. For example,
+intervals = [[1,4],[3,6],[7,9]][ [1,4], [3,6], [7,9] ]
+is sorted in terms of start times 1,31, 3, and 77.
+
+Your task is to merge the overlapping intervals
+and return a new output array consisting of only the non-overlapping intervals."""
