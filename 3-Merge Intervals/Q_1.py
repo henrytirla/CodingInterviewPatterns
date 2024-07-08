3df56be0f153237afa1bc4d0bcef4e7ea7fa30a3
Henry Tirla <henrytirla@gmail.com>
2024-07-08 18:18:43 +0200

    1- Merge Intervals

--- 3-Merge Intervals/Q_1.py
+++ 3-Merge Intervals/Q_1.py
@@ -6,3 +6,23 @@ is sorted in terms of start times 1,31, 3, and 77.
 
 Your task is to merge the overlapping intervals
 and return a new output array consisting of only the non-overlapping intervals."""
+
+class Solution:
+    def merge_intervals(self,intervals):
+        merged=[]
+        for interval in intervals:
+            if not merged or merged[-1][1]<interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1]=max(merged[-1][1],interval[1])
+        return merged
+
+def test_merge_intervals():
+    sol=Solution()
+    assert sol.merge_intervals([[1,4],[3,6],[7,9]])==[[1,6],[7,9]]
+    assert sol.merge_intervals([[1,4],[2,3],[4,6]])==[[1,6]]
+    assert sol.merge_intervals([[1,4],[5,6],[7,9]])==[[1,4],[5,6],[7,9]]
+    assert sol.merge_intervals([[1,4],[0,2],[3,5]])==[[1,5]]
+
+if __name__ == '__main__':
+    test_merge_intervals()
