702c196cf48d8dc44de1e443ee0c8556ea71db83
Henry Tirla <henrytirla@gmail.com>
2024-07-12 15:17:19 +0200

    4

---
+++ 3-Merge Intervals/Q_4.py
@@ -0,0 +1,2 @@
+"""You’re given a list containing the schedules of multiple employees. Each person’s schedule is a list of non-overlapping intervals in sorted order. An interval is specified with the start and end time, both being positive integers.
+Your task is to find the list of finite intervals representing the free time for all the employees."""
