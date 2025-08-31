8c2712b458c15ccc798fae8fe57308f18b3521f8
Henry Tirla <henrytirla@gmail.com>
2025-08-31 12:19:19 +0100

    Daily Grind

--- 0-Daily_Grind/1.py
+++ 0-Daily_Grind/1.py
@@ -5,6 +5,7 @@ Suppose we had an array [1, 3, 6, 7, 9], and let's say our "goal" number was 10.
 Our numbers to sum to it could be 3 and 7,
 and we would return an array of indices 1 and 3 respectively."""
 
+#(Sorted Array Target Sum)
 
 class Solution:
     def twoNumberSum(self,arr,targetSum:int):
