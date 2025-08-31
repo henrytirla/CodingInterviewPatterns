580cd27219d3fb3682a0e3905dc39f6766754cd1
Henry Tirla <henrytirla@gmail.com>
2025-08-31 13:42:37 +0100

    Daily Grind

--- 0-Daily_Grind/4.py
+++ 0-Daily_Grind/4.py
@@ -67,7 +67,24 @@ class Solution:
 
         return max_area
 
+""""
+Compact Solution
+def maxArea(self, height):
+    max_area = 0
+    i = 0
+    j = len(height) - 1
+    
+    while i < j:
+        max_area = max(max_area, min(height[i], height[j]) * (j - i))
+        if height[i] < height[j]:
+            i += 1
+        else:
+            j -= 1
+            
+    return max_area
 
+
+"""
 
 
 def test_maxArea():
