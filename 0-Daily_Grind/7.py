a7f6d0b290ae4880cd79f0cb12c26d4485b393e7
Henry Tirla <henrytirla@gmail.com>
2025-09-05 01:06:07 +0100

    Daily Grind

--- 0-Daily_Grind/7.py
+++ 0-Daily_Grind/7.py
@@ -1,5 +1,6 @@
 """
-Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+Given an array nums of n integers, return an array of all the unique
+quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
 
 0 <= a, b, c, d < n
 a, b, c, and d are distinct.
@@ -30,7 +31,37 @@ Constraints:
 
 class Solution:
      def Four_sum(self,arr:list[int],target:int)->list[int]:
-         pass
+         arr.sort()
+         result=[]
+         for i in range(len(arr)-3):
+             if i>0 and arr[i]== arr[i-1]:
+                 continue
+             for j in range(i+1,len(arr)-2):
+                 if j> i+1 and arr[j]== arr[j-1]:
+                     continue
+
+                 k = j+1
+                 n= len(arr)-1
+
+                 while k < n:
+                     current_sum= arr[i]+arr[j]+arr[k]+arr[n]
+
+                     if current_sum==target:
+                         result.append([arr[i],arr[j],arr[k],arr[n]])
+
+                         while k < n and  arr[k]==arr[k+1]:
+                             k+=1
+                         while k < n and arr[n]==arr[n-1]:
+                             n-=1
+                         k+=1
+                         n-=1
+                     elif current_sum<target:
+                          k+=1
+                     else:
+                         n-=1
+         return result
+
+
 
 
 
@@ -39,8 +70,12 @@ class Solution:
 def test_Four_sum():
     sol= Solution()
     assert sol.Four_sum([1,0,-1,0,-2,2],0)==[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
-    assert sol.Four_sum([2,2,2,2,2],8)==[[2,2,2,2]]
-    assert sol.Four_sum([1,2,3,4,5],50)==[]
+    assert sol.Four_sum([1, 2, 3, 4, 5, 6],12)==[[1, 2, 3, 6],[1, 2, 4, 5]]
+    assert sol.Four_sum([2, 2, 2, 2,2],8)==[[2,2,2,2]]
+    assert sol.Four_sum([-2,-1,-1,1,1,2,2], -0) == [[-2,-1,1,2],[-1,-1,1,1]]
+
+
+
 
 
 if __name__ == '__main__':
