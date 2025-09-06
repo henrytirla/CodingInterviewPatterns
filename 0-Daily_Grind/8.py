a4156b80c67bf47577e88a9311b430bfc4ccb459
Henry Tirla <henrytirla@gmail.com>
2025-09-06 16:37:10 +0100

    Daily Grind

--- 0-Daily_Grind/8.py
+++ 0-Daily_Grind/8.py
@@ -28,3 +28,27 @@ Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We ret
 
 
 """
+
+
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        left = 0
+        right=len(numbers)-1
+        while left < right:
+            current_sum=numbers[left]+numbers[right]
+            if current_sum==target:
+                return [left+1,right+1]
+            elif current_sum<target:
+                left+=1
+            else:
+                right-=1
+
+
+def test():
+    sol = Solution()
+    assert sol.twoSum([2, 7, 11, 15], 9) == [1, 2]
+    assert sol.twoSum([2, 3, 4], 6) == [1, 3]
+    assert sol.twoSum([-1, 0], -1) == [1, 2]
+
+if __name__ == '__main__':
+    test()
