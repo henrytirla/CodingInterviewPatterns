7959339a8c56b82db2cd7000c829ef5b9253cd31
Henry Tirla <henrytirla@gmail.com>
2025-09-08 23:47:29 +0100

    Daily Grind

--- 0-Daily_Grind/Two_pointers/Sorted Array Target Sum/Q_11.py
+++ 0-Daily_Grind/Two_pointers/Sorted Array Target Sum/Q_11.py
@@ -25,3 +25,23 @@ nums is sorted in non-decreasing order.
 Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
 
 """
+
+
+class Solution:
+    def sortedSquares(self, nums: list[int]) -> list[int]:
+
+        for i in range(len(nums)):
+            nums[i] = nums[i] ** 2
+        nums.sort()
+        return nums
+
+
+def test():
+    sol = Solution()
+    assert sol.sortedSquares([-4, -1, 0, 3, 10]) == [0, 1, 9, 16, 100]
+    assert sol.sortedSquares([-7, -3, 2, 3, 11]) == [4, 9, 9, 49, 121]
+    assert sol.sortedSquares([-5, -3, -2, -1]) == [1, 4, 9, 25]
+
+
+if __name__ == '__main__':
+    test()
